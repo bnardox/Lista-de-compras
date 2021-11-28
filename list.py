334a8687514a639ffba1abc2a268e767a1fb869f
Razzynt <eubernardoassis@gmail.com>
2021-11-28 19:04:26 -0300

    gerar arquivo txt inicio

--- list.py
+++ list.py
@@ -2,11 +2,18 @@ produtos = []
 produton = 0
 valorn = 1
 
+
+print('____________________________________')
+print()
+print('*         Lista de compras         *')
+print('____________________________________')
+print()
+
 while True:
     produto = str(input('Nome do produto: '))
     produtos.append(produto)
     
-    valor = float(input('Valor do produto: '))
+    valor = input('Valor do produto: ')
     produtos.append(valor)
 
     confirm = str(input('Quer adicionar outro produto?[Y/N] ')).lower()
@@ -21,8 +28,19 @@ def total():
         contador += 1
     return contador
 
-
 while total() > valorn:
-    print(f'Produto: {produtos[produton]}....... Valor: R${produtos[valorn]}')
+    print(f'\nProduto: {produtos[produton]}....... Valor: R${produtos[valorn]}')
     produton += 2
     valorn += 2
+
+
+gerar = str(input('\nVocê quer gerar um arquivo?'))
+if gerar == 'y':
+    arquivo = open('lista.txt', 'w')
+    produton = 0
+    valorn = 1
+    for p in produtos:
+        arquivo.write(f'Produto:{produtos[produton]} Valor: {produtos[valorn]}')
+        arquivo.write(' \n')
+        valorn += 2
+        produton += 2
